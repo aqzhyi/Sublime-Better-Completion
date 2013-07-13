dc240328d97bc5d63e19c0f2df3f4bc0f78b5cef
pleasurazy <pleasurazy@gmail.com>
2013-07-13 19:42:18 +0800

    Added completion extend list setting

--- main.py
+++ main.py
@@ -13,13 +13,16 @@ class PleasurazyAPICompletionsPackage(sublime_plugin.EventListener):
     for key in self.settings.get('completion_active_list'):
       self.api[key] = sublime.load_settings('API-completions-' + key + '.sublime-settings')
 
+    # Caching extended completions
+    for key in self.settings.get('completion_active_extend_list'):
+      self.api[key] = sublime.load_settings('API-completions-' + key + '.sublime-settings')
 
   def on_query_completions(self, view, prefix, locations):
     self.completions = []
 
     for key in self.api:
       # If completion active
-      if self.settings.get('completion_active_list').get(key):
+      if self.settings.get('completion_active_list').get(key) or self.settings.get('completion_active_extend_list').get(key):
         scope = self.api[key].get('scope')
         if scope and view.match_selector(locations[0], scope):
           self.completions += self.api[key].get('completions')
