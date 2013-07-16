980397c6afad07e16a9acc65df50d51222e99ff0
pleasurazy <pleasurazy@gmail.com>
2013-07-16 23:50:54 +0800

    Sublime Text 3 are load async.

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import sublime, sublime_plugin
 import string
 
 
-class PleasurazyAPICompletionsPackage(sublime_plugin.EventListener):
-
-  def __init__(self):
+class PleasurazyAPICompletionsPackage():
+  def init(self):
     self.api = {}
-    if not hasattr(self, 'settings'):
-      self.settings = sublime.load_settings('sublime-API-Completions-Package.sublime-settings')
+    self.settings = sublime.load_settings('sublime-API-Completions-Package.sublime-settings')
 
     # Caching completions
     for key in self.settings.get('completion_active_list'):
@@ -17,15 +15,30 @@ class PleasurazyAPICompletionsPackage(sublime_plugin.EventListener):
     for key in self.settings.get('completion_active_extend_list'):
       self.api[key] = sublime.load_settings('API-completions-' + key + '.sublime-settings')
 
+# In Sublime Text 3 things are loaded async, using plugin_loaded() callback before try accessing.
+pleasurazy = PleasurazyAPICompletionsPackage()
+
+if int(sublime.version()) < 3000:
+  pleasurazy.init()
+else:
+  def plugin_loaded():
+    global pleasurazy
+    pleasurazy.init()
+
+
+
+class PleasurazyAPICompletionsPackageEventListener(sublime_plugin.EventListener):
+  global pleasurazy
+
   def on_query_completions(self, view, prefix, locations):
     self.completions = []
 
-    for key in self.api:
+    for key in pleasurazy.api:
       # If completion active
-      if self.settings.get('completion_active_list').get(key) or self.settings.get('completion_active_extend_list').get(key):
-        scope = self.api[key].get('scope')
+      if pleasurazy.settings.get('completion_active_list').get(key) or pleasurazy.settings.get('completion_active_extend_list').get(key):
+        scope = pleasurazy.api[key].get('scope')
         if scope and view.match_selector(locations[0], scope):
-          self.completions += self.api[key].get('completions')
+          self.completions += pleasurazy.api[key].get('completions')
 
     if not self.completions:
       return []
